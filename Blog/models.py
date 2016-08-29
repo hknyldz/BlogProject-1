64994ece26e27251ceac4fe0c9e4167f70a7ecf2
aliymn <aliymn.db@gmail.com>
2016-08-29 13:45:26 +0300

    seo_url için slugify modülü kullanıldı. Makale başlığına ve kategori adına göre otomatik url oluşturuyor.

--- Blog/models.py
+++ Blog/models.py
@@ -2,20 +2,24 @@ from django.db import models
 from ckeditor.fields import RichTextField
 from imagekit.models import ProcessedImageField
 from imagekit.processors import ResizeToFill
-from django.core.exceptions import ValidationError
+from uuslug import slugify
 # Create your models here.
 class category(models.Model):
     category_name = models.CharField(max_length=50,null=True)
     category_keywords = models.CharField(max_length=500,null=True)
     category_description = models.CharField(max_length=500,null=True)
     category_icon = models.CharField(max_length=100,null=True)
-    seo_url = models.CharField(max_length=500,null=True,blank=True)
+    seo_url = models.CharField(max_length=500,unique=True, null=True, blank=True,verbose_name='Seo_URL : (Otomatik doldurur)')
     class Meta:
         verbose_name_plural = "Kategoriler"
 
     def __str__(self):
         return '{}'.format(self.category_name)
 
+
+    def save(self, *args, **kwargs):
+        self.seo_url = slugify(self.category_name)
+        super(category, self).save(*args, **kwargs)
 
 class post(models.Model):
     title = models.CharField(max_length=500,null=True)
@@ -28,13 +32,17 @@ class post(models.Model):
                                            format='JPEG',
                                            options={'quality': 60},null=True)
     category_list = models.ForeignKey(category,null=True)
-    seo_url = models.CharField(max_length=500,unique=True, null=True, blank=True)
+    seo_url = models.CharField(max_length=500,unique=True, null=True, blank=True,verbose_name='Seo_URL : (Otomatik doldurur)')
     is_active = models.BooleanField(default=False)
     class Meta:
         verbose_name_plural = "Blog Yaz"
 
     def __str__(self):
         return '{}'.format(self.title)
+
+    def save(self, *args, **kwargs):
+        self.seo_url = slugify(self.title)
+        super(post, self).save(*args, **kwargs)
 
 class content_media(models.Model):
     blog = models.ForeignKey(post)
